20f5ab89ac54567969afc42589e591de06b25858
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-05-21 19:12:13 +0300

    Initial Commit

---
+++ BGSoft/BGSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGSoftApp.swift
+//  BGSoft
+//
+//  Created by Artem Kutasevich on 21.05.22.
+//
+
+import SwiftUI
+
+@main
+struct BGSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
